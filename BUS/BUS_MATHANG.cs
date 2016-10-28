18e127aa2a72f90438b65dd569aa53b2db438e52
cuopemveday <nguyenhuuhuy1581995@gmail.com>
2016-10-28 10:20:52 +0700

    mat hang

---
+++ BUS/BUS_MATHANG.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using DTO;
+using System.Data;
+namespace BUS
+{
+     public class BUS_MATHANG
+    {
+         public static void Them_MatHang(MatHangInfo mh)
+         {
+             DAO_MATHANG.Them_MatHang(mh);
+         }
+         public static void Sua_MatHang(MatHangInfo mh)
+         {
+             DAO_MATHANG.Sua_MatHang(mh);
+         }
+         public static void Xoa_MatHang ( string MaMH)
+         {
+             DAO_MATHANG.Xoa_MatHang(MaMH);
+         }
+         public static DataTable HienThi_All()
+         {
+             return DAO_MATHANG.HienThi_MatHang_All();
+         }
+
+    }
+}
