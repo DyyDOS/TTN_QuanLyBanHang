04c30b5e18f3d45271f2decdeb0a5de45b85b2db
kcsmta <tungkhanhmta@gmail.com>
2016-10-30 10:15:09 +0700

    DAO Hoa Don

---
+++ DAO/DAO_HOADON.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.ApplicationBlocks.Data;
+using DTO;
+
+namespace DAO
+{
+    public class DAO_HOADON
+    {
+        public static SqlConnection con;
+        public static DataSet ds;
+        public static DataTable dt;
+        public static DataTable HienThi_HoaDon_All()
+        {
+            con = DAO_DATABASE.OpenConnect();
+
+            dt = SqlHelper.ExecuteDataset(con, "PR_HOADON_HIENTHITATCA").Tables[0];
+            DAO_DATABASE.CloseConnect(con);
+            return dt;
+        }
+    }
+}
